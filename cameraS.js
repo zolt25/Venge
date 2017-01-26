2ee0af9be4caf968944a5440a03ccef84bb9d3d1
Ivan Tomić <ivantomic75@gmail.com>
2017-01-26 20:02:00 +0100

    Update cameraS.js

--- cameraS.js
+++ cameraS.js
@@ -3,11 +3,12 @@
 private var player = "player";
 
 function Start () {
-
 }
 
 function Update () {
-
-    transform.position = new Vector3(GameObject.Find(player).transform.position.x,10,GameObject.Find(player).transform.position.z-8);
-
-    }
+    transform.position = new Vector3(
+        GameObject.Find(player).transform.position.x,
+        10,
+        GameObject.Find(player).transform.position.z-8
+    );
+}
