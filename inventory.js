c9c7ed2f6d87773697287701a92646d6763a48f9
Ivan Tomić <ivantomic75@gmail.com>
2017-01-26 20:16:12 +0100

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,20 +1,20 @@
 ﻿#pragma strict
 
 static var healthPacks = 0;
+
 private var player = "player";
 
 function Start () {
-
 }
 
-function OnCollisionEnter(collision: Collision){
+function OnCollisionEnter(collision: Collision) {
     if (collision.gameObject.FindWithTag("HealthPack") && healthPacks < 4) {
         healthPacks++;
         Debug.Log(healthPacks);
     }
+    
     return 0;
 }
 
 function Update () {
-
 }
