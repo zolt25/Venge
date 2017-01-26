fbe7637a395b6bf4b0e2c7b2090b4b1779ba2637
Ivan Tomić <ivantomic75@gmail.com>
2017-01-26 20:14:36 +0100

    Update healthPackS.js

--- healthPackS.js
+++ healthPackS.js
@@ -1,74 +1,87 @@
 ﻿#pragma strict
-
-public var player = "player";
-private var  negX : float;
-private var negZ : float;
-public var  attractSpeed : float = 0.2;
 public static var heal = 40;
 
-private function xPos() {
+public var player = "player";
+public var attractSpeed : float = 0.2;
 
+private var negX : float;
+private var negZ : float;
+
+private function xPos() {
     if (GameObject.Find(player).transform.position.x <= transform.position.x) {
         negX = -attractSpeed;
     } else {
         negX = attractSpeed;
     }
 
-    if (transform.position.x != GameObject.Find(player).transform.position.x)
-    {
+    if (transform.position.x != GameObject.Find(player).transform.position.x) {
         transform.position.x += negX;
     }
 
     return 0;
 
-        /*if (Mathf.Round(transform.position.x) != Mathf.Round(GameObject.Find(player).transform.position.x))
-    {
+    /*
+    if (Mathf.Round(transform.position.x) != Mathf.Round(GameObject.Find(player).transform.position.x)) {
         transform.position.x += negX;
     }
-*/
-
-    //do {transform.position.x += negativeX}
-    //while(Mathf.Round(transform.position.x) != Mathf.Round(GameObject.Find(player).transform.position.x));
-
+    */
+    
+    /*
+    do {
+        transform.position.x += negativeX
+    } while (Mathf.Round(transform.position.x) != Mathf.Round(GameObject.Find(player).transform.position.x));
+    */
 }
 
 private function zPos() {
-
     if (GameObject.Find(player).transform.position.z <= transform.position.z) {
         negZ = -attractSpeed;
     } else {
         negZ = attractSpeed;
     }
-        transform.position.z += negZ;
+    transform.position.z += negZ;
 
     return 0;
-
 }
 
 function Start () {
-
-
 }
 
 function Update () {
-
 // Priblizava se ako si u radijusu, fuckfest ukratko
-if (Mathf.Abs(GameObject.Find(player).transform.position.x) > Mathf.Abs(transform.position.x) || Mathf.Abs(GameObject.Find(player).transform.position.z) > Mathf.Abs(transform.position.z) ) {
-    if (Mathf.Abs(GameObject.Find(player).transform.position.x) - Mathf.Abs(transform.position.x) <= 4 && Mathf.Abs(GameObject.Find(player).transform.position.z) - Mathf.Abs(transform.position.z) <= 4) {
+if (
+    Mathf.Abs(GameObject.Find(player).transform.position.x) > Mathf.Abs(transform.position.x) || 
+    Mathf.Abs(GameObject.Find(player).transform.position.z) > Mathf.Abs(transform.position.z)
+) {
+    if (
+        Mathf.Abs(GameObject.Find(player).transform.position.x) - Mathf.Abs(transform.position.x) <= 4 && 
+        Mathf.Abs(GameObject.Find(player).transform.position.z) - Mathf.Abs(transform.position.z) <= 4
+    ) {
         xPos();
         zPos();
     }
-} else if (Mathf.Abs(GameObject.Find(player).transform.position.x) < Mathf.Abs(transform.position.x) || Mathf.Abs(GameObject.Find(player).transform.position.z) < Mathf.Abs(transform.position.z)) {
-    if (Mathf.Abs(transform.position.x) - Mathf.Abs(GameObject.Find(player).transform.position.x) <= 4 && (Mathf.Abs(transform.position.z) - Mathf.Abs(GameObject.Find(player).transform.position.z) <= 4)) {
+} else if (
+    Mathf.Abs(GameObject.Find(player).transform.position.x) < Mathf.Abs(transform.position.x) || 
+    Mathf.Abs(GameObject.Find(player).transform.position.z) < Mathf.Abs(transform.position.z)
+) {
+    if (
+        Mathf.Abs(transform.position.x) - Mathf.Abs(GameObject.Find(player).transform.position.x) <= 4 && 
+        Mathf.Abs(transform.position.z) - Mathf.Abs(GameObject.Find(player).transform.position.z) <= 4
+    ) {
         xPos();
         zPos();
     }
 }
     
-    //if (Mathf.Abs(GameObject.Find(player).transform.position.x) - Mathf.Abs(transform.position.x) <= -2 || Mathf.Abs(GameObject.Find(player).transform.position.z) <= -2) {
-    //    xPos();
-    //    zPos();
-    //}
+    /*
+    if (
+        Mathf.Abs(GameObject.Find(player).transform.position.x) - Mathf.Abs(transform.position.x) <= -2 || 
+        Mathf.Abs(GameObject.Find(player).transform.position.z) <= -2
+    ) {
+        xPos();
+        zPos();
+    }
+    */
 
 }
 
@@ -76,5 +89,6 @@ function OnCollisionEnter(col: Collision){
     if (col.gameObject.tag.Equals("Player") == true && inventory.healthPacks < 4) {
         Destroy(gameObject);
     }
+    
     return 0;
 }
